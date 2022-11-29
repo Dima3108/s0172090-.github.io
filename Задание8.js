eb5e09a8153e9a2dee561529f061c6c8c2c13b7b
Dima <73338242+Dima3108@users.noreply.github.com>
2022-11-29 20:52:35 +0300

    q3

--- Задание8.js
+++ Задание8.js
@@ -91,6 +91,11 @@ if (localStorage.getItem("user_comment") != null) {
     var us_c = document.getElementById("us_comment");
     us_c.value = localStorage.getItem("user_comment");
 }
+if (localStorage.getItem("cesh_err")!=null){
+    var m = document.getElementById("cesh_mes");
+    m.textContent = localStorage.getItem("cesh_err");
+    localStorage.removeItem("cesh_err");
+}
 if (window.location.search=="?op=1") {
     OPEN_FORM();
 
@@ -99,3 +104,48 @@ else {
     CLOSE_FORM();
   
 }
+function CONTAINS_CHARS(value){
+    var v=String(value);
+    if(v==null||v=="")
+    return false;
+    for(var i=0;i<v.length;i++)
+    {
+        if(v.charAt(i)!=' '){
+            return true;
+        }
+    }
+    return false;
+}
+var s_button = document.getElementById("b_sumb");
+//s_button.addEventListener("click",
+function a(){
+    let f = document.getElementById("out_message");
+    var us_n = document.getElementById("us_name");
+    if (!CONTAINS_CHARS(String(us_n.value))) {
+
+        localStorage.setItem("cesh_err", "имя пользователя недолжно быть пустым");
+        return false;
+    }
+    var us_com = document.getElementById("us_comment");
+    if (!CONTAINS_CHARS(String(us_com.value))) {
+        alert(CONTAINS_CHARS(String(us_com.value)));
+        localStorage.setItem("cesh_err", "Коментарий пользователя недолжен быть пустым");
+        return false;
+    }
+    var us_mail = document.getElementById("us_email");
+    if (us_mail.value == null || String(us_mail.value) == "") {
+
+
+        localStorage.setItem("cesh_err", "Email пользователя недолжен быть пустым");
+        return false;
+    }
+    var us_poli = document.getElementById("us_policy");
+    if (Boolean(us_poli.value) != true) {
+
+
+        localStorage.setItem("cesh_err", "Вы недали согласие на обработку данных");
+        return false;
+    }
+    return true;
+}
+//);
